4be2ec1be3e6b1efe9fbd6fc3ddd8edc9ce26f0c
LAPTOP-O5LPIPKP\tiago <tiago_honorio2010@hotmail.com>
2017-08-26 11:51:17 -0300

    start

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+/**
+ * Created by tiago on 26/08/2017.
+ */
+module.exports={
+    entry:[
+        './src/index.jsx'
+    ],
+    output:{
+        filename:'bundle.js',
+        path: ''
+    }
+};
